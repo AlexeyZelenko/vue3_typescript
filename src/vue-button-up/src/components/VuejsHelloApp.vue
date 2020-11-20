8ab3fe9613cab6291c08f2d1d628c77f2cc8f5ab
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-11-20 10:48:39 +0200

    initial commit

---
+++ src/vue-button-up/src/components/VuejsHelloApp.vue
@@ -0,0 +1,60 @@
+<template>
+  <a
+    :style='{ display: activeBlock }'
+    class="back_to_top"
+    title="Наверх"
+  >↑</a>
+</template>
+
+<script>
+export default {
+  name: 'ButtonTop',
+  data: () => ({
+    activeBlock: 'display'
+  }),
+  methods: {
+    trackScroll () {
+      const scrolled = window.pageYOffset
+      const coords = document.documentElement.clientHeight
+
+      if (scrolled > coords) {
+        this.activeBlock = 'block'
+      }
+      if (scrolled < coords) {
+        this.activeBlock = 'none'
+      }
+    },
+
+    backToTop () {
+      if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80)
+        setTimeout(this.backToTop, 0)
+      }
+    }
+  },
+  mounted () {
+    const goTopBtn = document.querySelector('.back_to_top')
+    window.addEventListener('scroll', this.trackScroll)
+    goTopBtn.addEventListener('click', this.backToTop)
+  },
+  watch: {
+    activeBlock () {
+      console.log('Данные изменились на:', this.activeBlock)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .back_to_top {
+    opacity: 0.8;
+    position: fixed;
+    bottom: 70px;
+    right: 40px;
+    z-index: 9999;
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    display: none
+  }
+</style>
