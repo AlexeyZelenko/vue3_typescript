c64ceb01ea626fdaa90bd114cc87df1bf743bd86
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-11-30 20:22:06 +0200

    version 1.1.4 Фотогалерея

--- src/views/CategoriesPhotoProfile.vue
+++ src/views/CategoriesPhotoProfile.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="flex flex-wrap justify-center">
-    <router-link to="/photo">
-      <svg
-        width="2em"
-        height="2em"
-        viewBox="0 0 16 16"
-        class="bi bi-arrow-left hover:text-blue-700"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"
-        />
-        <path
-          fill-rule="evenodd"
-          d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-        />
-      </svg>
-    </router-link>
     <div class="mx-4  max-w-4xl justify-center">
-      <h1 class="font-bold text-5xl flex-1 mb-4">Фотогалерея</h1>
+      <router-link to="/photo">
+        <span class="d-grid gap-2 flex-1 mb-4">
+          <button
+            style="margin-top: 10px"
+            class="btn btn-primary"
+            type="button"
+          >
+            <svg
+              width="2em"
+              height="2em"
+              viewBox="0 0 16 16"
+              class="bi bi-arrow-left hover:text-blue-700"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+            назад до Фотогалереї
+          </button>
+        </span>
+      </router-link>
       <template v-if="arrayImages">
         <PhotoList :category="arrayImages" />
       </template>
