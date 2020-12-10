a4a7156cf2475590c38b02c0b88c754d0abc6df1
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-12-10 19:19:44 +0200

    version 1.1.5

---
+++ src/components/photos/ModalImages.vue
@@ -0,0 +1,107 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-header">
+          <slot name="header" ></slot>
+          <div
+            type="button"
+            class="modal-close"
+          >
+            <svg
+              style="cursor: pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 352 512"
+            >
+              <path
+                fill="currentColor"
+                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div class="modal-container">
+
+          <div class="modal-body">
+            <slot name="body"></slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer"></slot>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'ModalImages',
+  props: {
+    photo: String
+  }
+}
+</script>
+
+<style>
+  .modal-header {
+    padding: 20px 20px 10px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+  .modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(8, 8, 8, 0.8);
+    display: table;
+  }
+
+  .modal-wrapper {
+    height: 80%;
+    vertical-align: middle;
+  }
+
+  .modal-container {
+    height: 640px;
+    width: 80%;
+    margin: 0px auto;
+    background-repeat: no-repeat, repeat;
+    background-position: center center;
+    border-radius: 2px;
+  }
+
+  .modal-header h4 {
+    margin-left: 20px;
+    height: 100%;
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 30px;
+    margin-top: 0;
+    color: #eff4f1;
+  }
+
+  .modal-close {
+    color: #ffffff;
+    width: 40px;
+    height: 40px;
+  }
+
+  .modal-default-button {
+    float: right;
+  }
+
+  @media (max-width: 767px) {
+    .modal-container {
+      height: 50%
+    }
+    .modal-header h4 {
+      font-size: 18px;
+    }
+  }
+</style>
--- src/components/photos/PhotoCard.vue
+++ src/components/photos/PhotoCard.vue
@@ -4,12 +4,32 @@
     class="bg-gray-800 radius-sm shadow-md rounded-md h-full bg-image hover-zoom hover-shadow"
   >
     <!--    Картинка-->
-    <a href="#!">
+    <a
+      href="#!"
+      @click="clickFoto"
+    >
       <img
         :src="isShown ? photo : null"
         class="h-full w-full object-cover rounded-md z-depth-3 w-100"
+        @click="openModal(photo)"
       />
     </a>
+
+    <ModalImages
+      v-if="showModal"
+      @click="showModal = false"
+    >
+      <template #header>
+        <h4>{{photo}}</h4>
+      </template>
+
+      <template #body>
+        <img
+          :src="photo"
+        />
+      </template>
+    </ModalImages>
+
   </div>
 
 </template>
@@ -17,13 +37,21 @@
 <script>
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 import useIntersectionObserver from '@/composables/useIntersectionObserver'
+import ModalImages from '@/components/photos/ModalImages'
 
 export default {
   name: 'PhotoCard',
   props: {
     photo: String
   },
-  setup () {
+  components: {
+    ModalImages
+  },
+  setup (props) {
+    const openModal = (photo) => {
+      showModal.value = true
+    }
+    const showModal = ref(false)
     const el = ref(null)
     const { observe, unobserve, isShown } = useIntersectionObserver()
 
@@ -36,7 +64,9 @@ export default {
     })
     return {
       el,
-      isShown
+      isShown,
+      showModal,
+      openModal
     }
   }
 }
