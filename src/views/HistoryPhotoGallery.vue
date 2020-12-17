abaad89c09f1dbc911353d30e312f9c2b46d306b
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-12-17 14:22:57 +0200

    version 1.1.6

--- src/views/HistoryPhotoGallery.vue
+++ src/views/HistoryPhotoGallery.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="mr-4 flex flex-wrap justify-center"
+    class="flex flex-wrap justify-center"
   >
-    <div class="mx-4 max-w-4xl justify-center">
+    <div class="mx-4  max-w-4xl justify-center">
       <h1
         style="margin-top: 10px"
         class="font-bold text-5xl flex-1 mb-4 justify-center"
